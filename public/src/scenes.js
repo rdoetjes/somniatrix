c17ed34d8a2fb46d55331f165c8ea7ba0f9748e9
Raymond Doetjes <rdoetjes@phonax.com>
2025-04-20 16:29:07 +0200

    d

--- public/src/scenes.js
+++ public/src/scenes.js
@@ -203,6 +203,7 @@ export const scenes = {
         - It will cause physical harm to the anyone who uses it.
         - The item will take many victims in a gruesome way.
         - <THE AI makes a 50% chance whether the protagonist is killed by the item or not>
+        - NO ELECTRIC CAN OPENER!!!
         ${CHOICE_PROMPT}`,
   },
 };
