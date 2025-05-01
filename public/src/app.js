86e7a99aedb53240a3c0538d92dff4363750743f
Raymond Doetjes <rdoetjes@phonax.com>
2025-05-01 08:51:04 +0200

    old model

--- public/src/app.js
+++ public/src/app.js
@@ -158,7 +158,7 @@ async function getPlot(plotKey, userChoice = null) {
     choicesContainer.innerHTML = "";
 
     const response = await client.chat.completions.create({
-        model: "gpt-4.5-turbo",
+        model: "gpt-4-turbo",
         messages: newMessages,
     });
 
